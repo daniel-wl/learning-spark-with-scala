65c615f64535f3edc93a9d5f22de42f93cf6930e
Daniel Long <daniel0long@gmail.com>
2019-12-07 15:26:39 -0500

    Objects

---
+++ scala_classes/src/main/scala/com/daniel_wl/objects/Examples.scala
@@ -0,0 +1,8 @@
+package com.daniel_wl.objects
+
+object Examples {
+  def Example: Unit = {
+    //you don't need to create an instance of an object to use it
+    val result = Objects.staticMethod
+  }
+}
---
+++ scala_classes/src/main/scala/com/daniel_wl/objects/Objects.scala
@@ -0,0 +1,9 @@
+package com.daniel_wl.objects
+
+//objects are what would be called static classes in other languages
+object Objects {
+  //all methods on objects are what would be called static methods
+  def staticMethod: String = {
+    "static method return value"
+  }
+}
